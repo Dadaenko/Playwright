f6fa8271d9a3dc894099053da567c86a42bd8d07
Daria Makarenko <darya.makarenko@gmail.com>
2026-03-17 20:13:38 +0200

    Initial commit

---
+++ tests/workingWithAPI.spec.ts
@@ -0,0 +1,32 @@
+import {test, expect} from 'playwright/test';
+import tags from '../test-data/tags.json'
+
+test.beforeEach(async ({page}) => {
+
+    await page.route('*/**/api/tags', async route => {
+        await route.fulfill({
+            body: JSON.stringify(tags)
+        })
+    })
+    
+    await page.route('*/**/api/articles*', async route =>{
+        const response = await route.fetch()
+        const responseBody = await response.json()
+        responseBody.articles[0].title = "This is a test title"
+        responseBody.articles[0].description = "This description is amazing. Just wow"
+
+        await route.fulfill({
+            body: JSON.stringify(responseBody)
+        })
+    })
+
+    await page.goto('https://conduit.bondaracademy.com/')
+    await page.waitForTimeout(500)
+})
+
+test('Validate the presence of Conduit icon', async ({page}) => {
+    await expect(page.locator('.navbar-brand')).toHaveText('conduit')
+    await page.waitForTimeout(500)
+    await expect(page.locator('app-article-list h1').first()).toContainText('This is a test title')
+    await expect(page.locator('app-article-list p').first()).toContainText('This description is amazing. Just wow')
+})
